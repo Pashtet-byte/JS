e6c7eee72981c4f23ea6d418b47e63b69c09a7a2
Pashtet-byte <mitinp15@gmail.com>
2026-03-19 10:26:12 +0300

    Add files via upload

---
+++ JS-ФИНАЛ/блочная область видимости.js
@@ -0,0 +1,9 @@
+if (true) {
+  let x = 10;
+  const y = 20;
+  var z = 30;
+}
+
+console.log(z); // 30
+// console.log(x); // ошибка
+// console.log(y); // ошибка
---
+++ JS-ФИНАЛ/функция с локальными переменными.js
@@ -0,0 +1,9 @@
+function test() {
+  let a = 5;
+  const b = 10;
+
+  console.log(a + b); // 15
+}
+
+test();
+// console.log(a); // ошибка
